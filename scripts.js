2c9ecbd33d4a660f496f15d8cf445de2869f97ac
Jonatas Cavalcante <149975346+Jonatas-Jc@users.noreply.github.com>
2024-03-26 10:39:08 -0300

    projeto completo

--- scripts.js
+++ scripts.js
@@ -2,9 +2,6 @@ const list = document.querySelector('ul')
 
 
 
-
-
-
 function showAll(item) {
     let newList = ''
 
@@ -13,10 +10,10 @@ function showAll(item) {
         newList +=
             `
         <li>
-        <img src="${product.src}" alt="x-bacon"> 
-        <p>${product.name}</p><br>
-        <p>R$ ${product.price}</p>
-    </li>
+             <img src="${product.src}" alt="x-bacon"> 
+             <p>${product.name}</p><br>
+             <p>R$ ${product.price}</p>
+        </li>
         `
 
         list.innerHTML = newList
@@ -26,20 +23,36 @@ function showAll(item) {
 
 
 
-
 function mapAll(){
    
    
-    const newValue = menuOptions.map(product => {
+    const newValue = menuOptions.map(product => ({
 
-            name:product.name,
+            ...product,  //spread operator - modifica apenas as alterações feitas
             price: product.price * 0.9,
-            vegan: product.vegan,
-            src: product.src
             
-        })
+        }))
+
+            showAll(newValue)
+    }
+
+
+    function sumAll() {
+        const totalSum = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+        console.log(totalSum)
+
+        list.innerHTML = `
+        <li>
+            <p>O valor Total Dos Itens é R$ ${totalSum}</p>
+         </li>
+        `
 
     }
 
 
-    console.log(newValue)    
+    function filterAll() {
+
+            const filterAllItems = menuOptions.filter(item => item.vegan)
+
+            showAll(filterAllItems)
+     }
